0725ea1449dedf6bd843d95c864245ea1e16893d
kontsio1 <kontsio18@gmail.com>
2022-12-13 13:41:32 +0200

    updated tests based on feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,6 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
 beforeEach(() => seed(testData));
-// jest.setTimeout(10000);
 
 afterAll(() => {
   if (db.end) db.end();
@@ -37,37 +36,38 @@ describe("5. GET /api/reviews:review_id", () => {
       .expect(200)
       .then(({ body }) => {
         const { review } = body;
-        expect(review).toEqual(
-          expect.objectContaining({
-            review_id: expect.any(Number),
-            title: expect.any(String),
-            review_body: expect.any(String),
-            designer: expect.any(String),
-            review_img_url: expect.any(String),
-            votes: expect.any(Number),
-            category: expect.any(String),
-            owner: expect.any(String),
-            created_at: expect.any(String),
-          })
-        );
+        console.log(review);
+        expect(review).toMatchObject({
+          review_id: 6,
+          title: "Occaecat consequat officia in quis commodo.",
+          category: "social deduction",
+          designer: "Ollie Tabooger",
+          owner: "mallionaire",
+          review_body:
+            "Fugiat fugiat enim officia laborum quis. Aliquip laboris non nulla nostrud magna exercitation in ullamco aute laborum cillum nisi sint. Culpa excepteur aute cillum minim magna fugiat culpa adipisicing eiusmod laborum ipsum fugiat quis. Mollit consectetur amet sunt ex amet tempor magna consequat dolore cillum adipisicing. Proident est sunt amet ipsum magna proident fugiat deserunt mollit officia magna ea pariatur. Ullamco proident in nostrud pariatur. Minim consequat pariatur id pariatur adipisicing.",
+          review_img_url:
+            "https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          created_at: "2020-09-13T12:19:28.077Z",
+          votes: 8,
+        });
       });
-  })
-  test("status:400, review_id is not a number", ()=> {
+  });
+  test("status:400, review_id is not a number", () => {
     return request(app)
-    .get("/api/reviews/EhWhatWasThatIdAgain")
-    .expect(400)
-    .then( (res) => {
-      const msg = res.body.msg;
-      expect(msg).toBe('Very Bad Request!');
-    })
-  })
-  test("status:404, review_id is a non-valid number", ()=> {
+      .get("/api/reviews/EhWhatWasThatIdAgain")
+      .expect(400)
+      .then((res) => {
+        const msg = res.body.msg;
+        expect(msg).toBe("Very Bad Request!");
+      });
+  });
+  test("status:404, review_id is a non-valid number", () => {
     return request(app)
-    .get("/api/reviews/1445")
-    .expect(404)
-    .then( (res) => {
-      const msg = res.body.msg;
-      expect(msg).toBe('Not Found!');
-    })
-  })
+      .get("/api/reviews/1445")
+      .expect(404)
+      .then((res) => {
+        const msg = res.body.msg;
+        expect(msg).toBe("Not Found!");
+      });
+  });
 });
